2ddfa25845280e46c4209426a033fa99901f24f0
skyjoozero <skyjoozero@naver.com>
2021-01-26 23:38:54 +0900

    20200126
    승리조건 함수 미완성
    버튼기능 추가하기
    오목 ai 원리 확인하기

--- main.py
+++ main.py
@@ -9,9 +9,30 @@ BLUE = (0, 0, 255)
 WHITE = (255, 255, 255)
 
 board = pygame.image.load("images//board.jpg")
+charBoard = [ # 0 : 알 없음 1 : 흑돌 2 : 백돌
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+]
 
-pisXPos = [[]] # [[xpos, yops, setpos]]
-
+pisXPos = [10 + (460 / 18) * i for i in range(19)]
+pisYPos = [10 + (460 / 18) * i for i in range(19)]
 
 run = True
 order = 1
@@ -36,13 +57,34 @@ def whereClick(xpos):
         return 'menu'
 
 def setPiecePos(xpos, ypos): # 바둑알 위치 조정 함수
-    pass
+    for x in pisXPos:
+        for y in pisYPos:
+            if x - (460 / 18) / 2 <= xpos < x + (460 / 18) / 2 and y - (460 / 18) / 2 <= ypos < y +(460 / 18) / 2:
+                return [x, y]
+            else:
+                pass
 
 def drawPiece(xpos, ypos, order):
     if order % 2 != 0:
         pygame.draw.circle(screen, BLACK, [xpos, ypos], 10)
     else:
         pygame.draw.circle(screen, WHITE, [xpos, ypos], 10)
+
+def ㅡShapeWin(): # ㅡ모양 검사
+    for y in range(19):
+        for x in range(15):
+            if charBoard[y][x] != 0:
+                if charBoard[y][x] == charBoard[y][x + 1] == charBoard[y][x + 2] == charBoard[y][x + 3] == charBoard[y][x + 4]:
+                    return ['win', charBoard[y][x]]
+                else:
+                    pass
+            else:
+                pass
+def lShapeWin(): #todo l모양 승리 조건함수 완성하기
+    pass
+
+def xShapeWin(): #todo 대각선 모양 승리조건함수 완성하기
+    pass
 
 if __name__ == '__main__':
     pygame.init()
@@ -58,11 +100,21 @@ if __name__ == '__main__':
             elif e.type == pygame.MOUSEBUTTONUP:
                 pos = list(pygame.mouse.get_pos())
                 if whereClick(pos[0]) == 'board':
-                    pieces.append([pos[0], pos[1], order])
+                    setPos = setPiecePos(pos[0], pos[1])
+                    pieces.append([setPos[0], setPos[1], order])
+                    x = pisXPos.index(setPos[0])
+                    y = pisYPos.index(setPos[1])
+                    if order % 2 != 0:
+                        charBoard[y][x] = 1
+                    else:
+                        charBoard[y][x] = 2
                     # drawPiece(pos[0], pos[1])
                     order += 1
                 else:
                     try:
+                        x = pisXPos.index(pieces[-1][0])
+                        y = pisYPos.index(pieces[-1][1])
+                        charBoard[y][x] = 0
                         del pieces[-1]
                         order -= 1
                     except:
@@ -70,5 +122,17 @@ if __name__ == '__main__':
             setWindow()
             for p in pieces:
                 drawPiece(p[0], p[1], p[2])
+            result = ㅡShapeWin()
+            try:
+                if result[0] == 'win':
+                    if result[1] == 1:
+                        print('흑 승리')
+                    else:
+                        print('백 승리')
+                    run = False
+                else:
+                    pass
+            except:
+                pass
 
         pygame.display.update()
