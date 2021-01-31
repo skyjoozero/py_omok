9d77e71beb3a9bd0effa1e13105801366925ddbf
skyjoozero <skyjoozero@naver.com>
2021-01-31 23:20:00 +0900

    기본기능 완성

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 WHITE = (255, 255, 255)
 
-board = pygame.image.load("images//board.jpg")
 charBoard = [ # 0 : 알 없음 1 : 흑돌 2 : 백돌
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -61,7 +60,7 @@ def setWindow(): #todo 버튼 그래픽 추가 (undo, redo, numbering, new game,
         pygame.draw.line(screen, BLACK, [10, 10 + (460 / 18) * i], [470, 10 + (460 / 18) * i])
     return screen
 
-def renderMenu(): #todo undoMenu, (531, 315, 58, 23), redoMenu, (532, 348, 57, 23), numberingMenu, (499, 381, 122, 23), newGameMenu, (504, 414, 112, 23), quitMenu, (536, 447, 48, 23)
+def renderMenu(): #todo undoMenu, (531, 315, 58, 23), redoMenu, (532, 348, 57, 23), numberingMenu, (499, 381, 122, 23), newGameMenu, (504, 414, 112, 23), quit, (536, 447, 48, 23)
     undoMenu = menuFont.render("UNDO", True, BLACK)
     redoMenu = menuFont.render("REDO", True, BLACK)
     numberingMenu = menuFont.render("NUMBERING", True, BLACK)
@@ -90,10 +89,8 @@ def setPiecePos(xpos, ypos): # 바둑알 위치 조정 함수
 def drawPiece(xpos, ypos, order):
     if order % 2 != 0:
         pygame.draw.circle(screen, BLACK, [xpos, ypos], 10)
-        pprint.pprint(tempBoard)
     else:
         pygame.draw.circle(screen, WHITE, [xpos, ypos], 10)
-        pprint.pprint(tempBoard)
 
 def ㅡShapeWin(): # ㅡ모양 검사
     for y in range(19):
@@ -120,10 +117,15 @@ def xShapeWin():
     for y in range(15):
         for x in range(15):
             if tempBoard[y][x] != 0:
-                if tempBoard[y][x] == tempBoard[y + 1][x + 1] == tempBoard[y + 2][x + 2] == tempBoard[y + 3][x + 3] == tempBoard[y + 4][x + 4]: #todo \방향 검사
+                if tempBoard[y][x] == tempBoard[y + 1][x + 1] == tempBoard[y + 2][x + 2] == tempBoard[y + 3][x + 3] == \
+                        tempBoard[y + 4][x + 4]:  #todo \방향 검사
                     return ['win', tempBoard[y][x]]
-                elif tempBoard[y + 4][x] == tempBoard[y - 1][x + 1] == tempBoard[y - 2][x + 2] == tempBoard[y - 3][x + 3] == tempBoard[y - 4][x + 4]: #todo /방향 검사
-                    return ['win', tempBoard[y][x]]
+                else:
+                    pass
+            elif tempBoard[y + 4][x] != 0:
+                if tempBoard[y + 4][x] == tempBoard[y + 3][x + 1] == tempBoard[y + 2][x + 2] == \
+                        tempBoard[y + 1][x + 3] == tempBoard[y][x + 4]:  # todo /방향 검사
+                    return ['win', tempBoard[y + 4][x]]
                 else:
                     pass
             else:
@@ -133,18 +135,54 @@ def calResult():
     r = ㅡShapeWin() or lShapeWin() or xShapeWin()
     return r
 
+def resetBoard():
+    global pieces
+    pieces = []
+    for x in range(19):
+        for y in range(19):
+            charBoard[y][x] = 0
+
+redoList = []
+
+def undo():
+    global order
+    try:  # undo기능
+        x = pisXPos.index(pieces[-1][0])
+        y = pisYPos.index(pieces[-1][1])
+        charBoard[y][x] = 0
+        redoList.append(pieces[-1])
+        del pieces[-1]
+        order -= 1
+    except:
+        pass
+
+def redo():
+    try:
+        pieces.append(redoList[-1])
+        del redoList[-1]
+    except:
+        pass
+
 def menuClick(ypos):
     global numberingRun
+    global tempBoard
     if 315 <= ypos <= 338:
+        print("clicked undo")
         undo()
     elif 348 <= ypos <= 371:
+        print("clicked redo")
         redo()
     elif 381 <= ypos <= 404:
+        print("clicked numbering")
         numberingRun = not numberingRun
     elif 414 <= ypos <= 437:
-        tempBoard = charBoard
-    elif 447 <= ypos <= 70:
-        redo()
+        print("clicked new game")
+        numberingRun = False
+        resetBoard() #todo 다시하기 기능 만들기
+    elif 447 <= ypos <= 470:
+        print("clicked quit")
+        global run
+        run = False
 
 if __name__ == '__main__':
     pygame.init()
@@ -173,32 +211,18 @@ if __name__ == '__main__':
                             tempBoard[y][x] = 1
                             pieces.append([setPos[0], setPos[1], order])
                             order += 1
+                            redoList = []
                         elif order % 2 == 0 and tempBoard[y][x] == 0:
                             tempBoard[y][x] = 2
                             pieces.append([setPos[0], setPos[1], order])
                             order += 1
+                            redoList = []
                     except:
                         pass
                     else:
                         pass
                 else: #todo 메뉴클릭구현
                         menuClick(pos[1])
-                    # print(redoMenu.get_rect()) ##################### 메뉴구역설정 방법
-                    # my_text = my_font.render("STRING", 1, (0, 0, 0))
-                    # text_width = my_text.get_width()
-                    # text_height = my_text.get_height()
-                    #
-                    # screen.blit(my_text, (screen_width // 2 - text_width // 2, screen_height // 2 - text_height // 2)
-
-                    # try: # undo기능
-                    #     x = pisXPos.index(pieces[-1][0])
-                    #     y = pisYPos.index(pieces[-1][1])
-                    #     charBoard[y][x] = 0
-                    #     del pieces[-1]
-                    #     order -= 1
-                    # except:
-                    #     pass
-
             setWindow()
             for p in pieces:
                 drawPiece(p[0], p[1], p[2])
